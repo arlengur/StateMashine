d79cba8966e9936ea648778fe45eb4a94f6e8df8
Alexey Galin <agalin@natera.com>
2019-09-13 20:21:16 +0300

    v.0.2 actions added

--- src/main/java/ru/arlen/config/StateMachineConfig.java
+++ src/main/java/ru/arlen/config/StateMachineConfig.java
@@ -1,7 +1,9 @@
 package ru.arlen.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -15,7 +17,7 @@ import static ru.arlen.statemachine.States.*;
 
 @Configuration
 @EnableStateMachine
-
+@Slf4j
 public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States, Events> {
     @Override
     public void configure(StateMachineTransitionConfigurer<States, Events> transitions) throws Exception {
@@ -42,7 +44,21 @@ public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States
 
     @Override
     public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
-        states.withStates().initial(BACKLOG).state(IN_PROGRESS).state(TESTING).state(DONE);
+        states.withStates().initial(BACKLOG)
+                .state(IN_PROGRESS, timeToWork(), timeToSleep())
+                .state(TESTING, deployAction()).state(DONE);
+    }
+
+    private Action<States, Events> timeToSleep() {
+        return context -> log.warn("Time to sleep...");
+    }
+
+    private Action<States, Events> timeToWork() {
+        return context -> log.warn("Time to work...");
+    }
+
+    private Action<States, Events> deployAction() {
+        return context -> log.warn("Deploing...");
     }
 
     @Override
